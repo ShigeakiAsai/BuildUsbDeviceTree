ab6e357b8cc51fb1f6ead4d00bcaed397db77646
ShigeakiAsai <asai@cocoa.ocn.ne.jp>
2023-04-11 17:43:10 +0900

    Separate between main module and function module.

--- dnptreedata.h
+++ dnptreedata.h
@@ -1,6 +1,19 @@
 
 #pragma	once
 
+typedef struct _TARGET_USB_DEVICE_INFO {
+	USHORT      nTargetidVendor;
+	USHORT      nTargetidProduct;
+	CAtlString  strTargetiSerialNumber;
+	USHORT      nFindTargetNumber;
+	ULONG       nTargetDeviceSpeed_Usb110;
+	ULONG       nTargetDeviceSpeed_Usb200;
+	ULONG       nTargetDeviceSpeed_Usb300;
+	ULONG       nTargetDeviceDeviceIsOperatingAtSuperSpeedOrHigher;
+	ULONG       nTargetDeviceDeviceIsSuperSpeedCapableOrHigher;
+	ULONG       nTargetDeviceDeviceIsOperatingAtSuperSpeedPlusOrHigher;
+	ULONG       nTargetDeviceDeviceIsSuperSpeedPlusCapableOrHigher;
+} TARGET_USB_DEVICE_INFO, * PTARGET_USB_DEVICE_INFO;
 
 template<class VALUE_TYPE>
 class	CDnpTreeData
